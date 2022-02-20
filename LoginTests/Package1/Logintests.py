3c7cdd2b2aabbe8ffd78ff68947fb770c8947816
Chris <ceharrison@live.com>
2022-02-20 07:59:49 -0600

    clean up

--- LoginTests/Package1/Logintests.py
+++ LoginTests/Package1/Logintests.py
@@ -5,16 +5,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-username = 'ceharrison@live.com'  # use valid test login email
-password = 'Des515Moines'  # use valid test password
+username = ''  # use valid test login email
+password = ''  # use valid test password
 invalidUsername = 'unkownuserxyz@somedomain.com'  # use invalid test login
 invalidPassword = '1nVal1dP@ssw0rd'  # use invalid test password
 driver = webdriver.Chrome(executable_path="c:\webdrivers\chromedriver.exe")
 
 
 class LoginScenarios(unittest.TestCase):
-    # driver = webdriver.Chrome(executable_path="c:\webdrivers\chromedriver.exe")
-
     def test_successful_login(self):
         driver.get("http://hudl.com")
         driver.implicitly_wait(1500)
@@ -31,8 +29,7 @@ class LoginScenarios(unittest.TestCase):
         assert 'Home - Hudl' in driver.title
         driver.find_element(By.CLASS_NAME, 'hui-globaluseritem__avatar'), NoSuchElementException
         self.tearDown()
-        # print('success login')
-        # self.assertTrue(True)
+
 
     def test_invalid_username(self):
         driver.get("http://hudl.com")
@@ -50,8 +47,7 @@ class LoginScenarios(unittest.TestCase):
         assert 'Log In - Hudl' in driver.title
         driver.find_element(By.CLASS_NAME, 'login-error-container'), NoSuchElementException
         self.tearDown()
-        # print('invalid username test')
-        # self.assertTrue(True)
+
 
     def test_invalid_password(self):
         driver.get("http://hudl.com")
@@ -69,8 +65,6 @@ class LoginScenarios(unittest.TestCase):
         assert 'Log In - Hudl' in driver.title
         driver.find_element(By.CLASS_NAME, 'login-error-container'), NoSuchElementException
         self.tearDown()
-        # print('invalid password test')
-        # self.assertTrue(True)
 
 
 if __name__ == '__main__':
